97afe99281b9b84fdce17a4b92f04face1537ebf
root <root@shashiudawa5.mylabserver.com>
2018-02-27 04:35:13 +0000

    delete swp

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -1,8 +1,12 @@
-class ambari::agent::config() {
+class ambari::agent::config(
+  String $server_hostname   = $::ambari::server_hostname,
+  Integer $url_port         = $::ambari::url_port,
+  Integer $secured_url_port = $::ambari::secured_url_port,
+) {
   file { '/etc/ambari-agent/conf/ambari-agent.ini':
     ensure  => 'file',
-    owner   =>
-    group   =>
-    mode    =>
+    owner   => 'root',
+    group   => 'root'
+    mode    => '0644',
     content => template("${module_name}/ambari-agent.ini.erb"),
 }
